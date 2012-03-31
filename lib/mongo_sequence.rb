bd801ff58c1662fe49dd0293469898239787a36a
Brian Hempel <bnhempel@mtu.edu>
2012-03-31 02:37:50 -0400

    A bit of cleanup

--- lib/mongo_sequence.rb
+++ lib/mongo_sequence.rb
@@ -39,30 +39,30 @@ class MongoSequence
   end
 
   def next
-    current_after_operation(:$inc => { :current => 1 })
+    current_after_update(:$inc => { :current => 1 })
   end
 
   def current
-    current_after_operation(:$set => {}) # noop that works
+    current_after_update(:$set => {}) # noop that works
   end
 
   def current=(integer)
-    current_after_operation(:current => integer)
+    current_after_update(:current => integer)
   end
 
   private
 
-  def current_after_operation(operation)
-    collection.find_and_modify(
+  def current_after_update(update)
+    options = {
       :query  => { :_id => name },
       :new    => true, # return the modified doc
-      :update => operation,
-      :upsert => false
-    )['current']
+      :update => update
+    }
+    collection.find_and_modify(options)['current']
   rescue Mongo::OperationFailure => e
     raise unless e.message =~ /No matching object found/
     init_in_database
-    current_after_operation(operation)
+    current_after_update(update)
   end
 
   def init_in_database
